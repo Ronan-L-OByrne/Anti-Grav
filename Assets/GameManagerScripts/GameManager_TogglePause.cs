81ae522a1cdb63c5baa0e3b93584d3a4b2ac994f
Ronan O'Byrne <ronan004@gmail.com>
2017-03-28 15:05:03 +0100

    Separated assets into several subfolders, began implementing a Game Manager to handle various common events

---
+++ Assets/GameManagerScripts/GameManager_TogglePause.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager_TogglePause : MonoBehaviour
+{
+    private GM_Master gameManagerMaster;
+    private bool isPaused;
+
+    private void OnEnable()
+    {
+
+    }//end OnEnable()
+
+    private void OnDisable()
+    {
+
+    }//end OnDisable()
+
+    private void SetInitReferences()
+    {
+        gameManagerMaster = GetComponent<GM_Master>();
+    }//end SetInitReferences()
+    
+    private void TogglePause()
+    {
+        if(isPaused)
+        {
+            Time.timeScale = 1;
+            isPaused = false;
+        }//end if()
+        else
+        {
+            Time.timeScale = 0;
+            isPaused = true;
+        }//end else
+    }//end TogglePause()
+}//end class GameManager_TogglePause
---
+++ Assets/MasterScripts/GM_Master.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GM_Master : MonoBehaviour
+{
+    public delegate void GM_EventHandler();
+    public event GM_EventHandler MenuToggleEvent;
+    public event GM_EventHandler InventoryUIEvent;
+    public event GM_EventHandler RestartLevelEvent;
+    public event GM_EventHandler GoToMenuSceneEvent;
+    public event GM_EventHandler GameOverEvent;
+
+    public bool GameOver;
+    public bool InventoryUIOn;
+    public bool MenuOn;
+
+    public void CallEventMenuToggle()
+    {
+        if(MenuToggleEvent != null)
+        {
+            MenuToggleEvent();
+        }//end if
+    }//end CallEventMenuToggle()
+    
+    public void CallEventInventoryUI()
+    {
+        if (InventoryUIEvent != null)
+        {
+            InventoryUIEvent();
+        }//end if
+    }//end CallInventoryUIEvent()
+
+    public void CallEventRestartLevel()
+    {
+        if (RestartLevelEvent != null)
+        {
+            RestartLevelEvent();
+        }//end if
+    }//end CallRestartLevelEvent()
+
+    public void CallEventGoToMenuScene()
+    {
+        if (GoToMenuSceneEvent != null)
+        {
+            GoToMenuSceneEvent();
+        }//end if
+    }//end CallGoToMenuSceneEvent()
+
+    public void CallEventGameOver()
+    {
+        if (GameOverEvent != null)
+        {
+            GameOver = true;
+            GameOverEvent();
+        }//end if
+    }//end CallGoToMenuSceneEvent()
+}//end class GM_Master
