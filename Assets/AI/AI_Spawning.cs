4040ec09016bb62441dd22af073c045e1c11e8e0
Ronan O'Byrne <ronan004@gmail.com>
2017-04-23 14:09:45 +0100

    Added Enemy Spawing which occurs every 5 seconds. (Various other Rough/Unfinished changes)

---
+++ Assets/AI/AI_Spawning.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI_Spawning : MonoBehaviour
+{
+    private AI_Master AIManagerMaster;
+    private GameObject initEnemy;
+    private GameObject cameraChk;
+
+    private void OnEnable()
+    {
+        SetInitReferences();
+
+        AIManagerMaster.SpawnEnemyEvent += SpawnEnemy;
+    }//end OnEnable()
+
+    private void OnDisable()
+    {
+        AIManagerMaster.SpawnEnemyEvent -= SpawnEnemy;
+    }//end OnDisable()
+
+    private void SetInitReferences()
+    {
+        AIManagerMaster = GameObject.Find("Enemy_EvilPlayer").GetComponent<AI_Master>();
+    }//end setInitReferences()
+
+    private void Start()
+    {
+        AIManagerMaster = this.GetComponent<AI_Master>();
+        initEnemy = GameObject.Find("Enemy_EvilPlayer");
+        cameraChk = GameObject.Find("Main Camera");
+        InvokeRepeating("CallSpawnEnemy", 0.0f, 5.0f);
+    }//end Start()
+
+    public void CallSpawnEnemy()
+    {
+        SpawnEnemy(new Vector3(cameraChk.transform.position.x + 10, cameraChk.transform.position.y, initEnemy.transform.position.z));
+    }//end CallSpawnEnemy()
+
+    public void SpawnEnemy(Vector3 spawnPos)
+    {
+        GameObject newEnemy = Object.Instantiate(initEnemy, spawnPos, Quaternion.Euler(0, 0, 0));
+        newEnemy.transform.parent = gameObject.transform;
+        this.gameObject.SetActive(true);
+    }//end SpawnEnemy()
+}//end class AI_Spawning
--- Assets/MasterScripts/AI_Master.cs
+++ Assets/MasterScripts/AI_Master.cs
@@ -6,12 +6,22 @@ public class AI_Master : MonoBehaviour
 {
     public delegate void AI_HealthHandler(int healthChange);
     public event AI_HealthHandler DeductHealthEvent;
+    public delegate void AI_SpawnHandler(Vector3 spawnPos);
+    public event AI_SpawnHandler SpawnEnemyEvent;
 
     public void CallEventDeductHealth(int healthChange)
     {
         if (DeductHealthEvent != null)
         {
             DeductHealthEvent(healthChange);
+        }//end if
+    }//end CallEventDecreaseHealth()
+
+    public void CallEventSpawnEnemy(Vector3 spawnPos)
+    {
+        if (SpawnEnemyEvent != null)
+        {
+            SpawnEnemyEvent(spawnPos);
         }//end if
     }//end CallEventDecreaseHealth()
 }//end class AI_Master
