182779383b1cee4659652860b6911179030c2c24
Ronan O'Byrne <ronan004@gmail.com>
2017-04-24 10:36:05 +0100

    Implemented Health Spawn script which is called whenever the user travels 100 units in a direction.

--- Assets/Player/HealthPickUp.cs
+++ Assets/Player/HealthPickUp.cs
@@ -6,9 +6,17 @@ public class HealthPickUp : MonoBehaviour
 {
     public GameObject player;
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void Start()
     {
-        player.GetComponent<PM_Master>().CallEventIncreaseHealth(1);
-        Destroy(this.gameObject);
+        player = GameObject.Find("Player");
+    }//end Start()
+
+    private void OnCollisionEnter2D(Collision2D collisionOther)
+    {
+        if (collisionOther.gameObject.name == "Player")
+        {
+            player.GetComponent<PM_Master>().CallEventIncreaseHealth(1);
+            Destroy(this.gameObject);
+        }//end if
     }//end OnCollisionEnter2D()
 }//end class HealthPickUp
---
+++ Assets/Player/HealthSpawn.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthSpawn : MonoBehaviour
+{
+    public GameObject healthTemplate;
+    public GameObject cameraChk;
+    private bool spawnChk;
+
+    private void Start()
+    {
+        healthTemplate = GameObject.Find("HealthUp");
+        cameraChk = GameObject.Find("Main Camera");
+        spawnChk = true;
+        //InvokeRepeating("CallSpawnHealth", 0.0f, 5.0f);
+    }//end Start()
+
+    private void Update()
+    {
+        Debug.Log(cameraChk.transform.position.x % 100 + " - " + cameraChk.transform.position.x % 100);
+        if(cameraChk.transform.position.x % 100 < 5 && cameraChk.transform.position.x % 100 > -5)
+        {
+            if (spawnChk)
+            {
+                CallSpawnHealth();
+                spawnChk = false;
+            }//end if
+        }//end if
+        else
+        {
+            spawnChk = true;
+        }//end else
+    }//end Update()
+
+    private void CallSpawnHealth()
+    {
+        SpawnHealth(new Vector3(cameraChk.transform.position.x + 10, -3.5f, healthTemplate.transform.position.z));
+    }//end CallSpawnHealth()
+
+    private void SpawnHealth(Vector3 spawnPos)
+    {
+        GameObject newHealthPk = Object.Instantiate(healthTemplate, spawnPos, Quaternion.Euler(0, 0, 0));
+        newHealthPk.transform.parent = gameObject.transform;
+        this.gameObject.SetActive(true);
+    }//end SpawnHealth()
+
+    /*
+    private void Start()
+    {
+        initEnemy = GameObject.Find("Enemy_EvilPlayer");
+        cameraChk = GameObject.Find("Main Camera");
+        InvokeRepeating("CallSpawnEnemy", 0.0f, 5.0f);
+    }//end Start()
+
+    public void CallSpawnEnemy()
+    {
+        SpawnEnemy(new Vector3(cameraChk.transform.position.x + 10, -4, initEnemy.transform.position.z));
+    }//end CallSpawnEnemy()
+
+    public void SpawnEnemy(Vector3 spawnPos)
+    {
+        GameObject newEnemy = Object.Instantiate(initEnemy, spawnPos, Quaternion.Euler(0, 0, 0));
+        newEnemy.transform.parent = gameObject.transform;
+        this.gameObject.SetActive(true);
+    }//end SpawnEnemy()
+      
+    */
+}//end class HealthSpawn
